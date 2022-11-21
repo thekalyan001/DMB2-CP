b672a939d19e21c47b744c4a11dab6deecbbd3c5
Kalyan Mishra <55200919+thekalyan001@users.noreply.github.com>
2022-11-21 17:01:03 +0530

    Add files via upload

---
+++ 06. Dynamic programming/04. Unique partitions of number.cpp
@@ -0,0 +1,19 @@
+https://practice.geeksforgeeks.org/problems/unique-partitions1041/1
+
+void solve(int i, int n, int sum, vectro<int>&v, vector<int>temp, vector<vector<int>>&ans){
+	    if(sum>n) return;
+	    if(i>=n){
+	        if(sum==n) ans.push_back(temp); return;
+	    }
+	    
+	    temp.push_back(v[i]); //take current val
+	    solve(i, n, sum+v[i], v, temp, ans); //include then take the sum too
+	    temp.pop_back();
+	    solve(i+1, n, sum, v, temp, ans); //didn't include then dont take into sum
+	}
+    vector<vector<int>> UniquePartitions(int n) {
+        vector<int>v, temp;
+        vector<vector<int>>ans;
+        solve(0, n, 0, v, temp, ans) //i, n, sum, temp, ans
+        return ans;
+    }
